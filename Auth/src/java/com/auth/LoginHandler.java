fdb1290d4da73406001a10c9b1f0d8ce564f97f5
kajal <kajal@gmail.com>
2023-11-07 21:22:15 +0530

    ..

--- Auth/src/java/com/auth/LoginHandler.java
+++ Auth/src/java/com/auth/LoginHandler.java
@@ -10,9 +10,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
+import com.model.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import com.utility.PasswordHashing;
 
 /**
  *
@@ -22,12 +23,24 @@ public class LoginHandler  extends HttpServlet{
      protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        
+        password = PasswordHashing.doHash(password);
         
         //password checking
+        HttpSession session = req.getSession();
+        User user= (User)(session.getAttribute("user"));
+        String  Hashedpassword = user.getPassword();
+        if(Hashedpassword.equals(password)){
+            RequestDispatcher rd = req.getRequestDispatcher("Home.html");
+            rd.forward(req, res);
+        }
+        else{
+            RequestDispatcher rd = req.getRequestDispatcher("Error.html");
+            rd.forward(req, res);
+        }
         
-        RequestDispatcher rd = req.getRequestDispatcher("Home.html");
-        rd.forward(req, res);
+        
+        
+        
         
        
     }
